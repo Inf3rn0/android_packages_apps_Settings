f0522c4407254adf9a282e61e7472012662e3474
scott <scott@ubuntu>
2015-12-02 20:40:57 -0500

    Fix recents and move items from display to recents menu
    Change-Id: I128c25f7bec144f6b48eb9dca611395ac734457d
    Signed-off-by: scott <scott@ubuntu>

--- src/com/android/settings/cyanogenmod/Recents.java
+++ src/com/android/settings/cyanogenmod/Recents.java
@@ -79,6 +79,12 @@ public class Recents extends SettingsPreferenceFragment implements
     }
 
     @Override
+    protected int getMetricsCategory() {
+        // todo add a constant in MetricsLogger.java
+        return MetricsLogger.MAIN_SETTINGS;
+    }
+
+    @Override
     public boolean onPreferenceChange(Preference preference, Object newValue) {
         ContentResolver resolver = getActivity().getContentResolver();
         if (preference == mRecentsClearAllLocation) {
