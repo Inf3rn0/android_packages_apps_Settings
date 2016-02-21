e7c3f8813c14d7620271468fb6b73bc89711de0e
Inf3rn0 <gary@infnet.nl>
2016-02-21 10:03:59 +0100

    Revert "Powermenu screenshot delay [2/2]"
    This reverts commit 554b27a605a24e49cb652d0a5af820d3e0eaddb4.

--- src/com/android/settings/cyanogenmod/PowerMenuActions.java
+++ src/com/android/settings/cyanogenmod/PowerMenuActions.java
@@ -17,7 +17,6 @@
 package com.android.settings.cyanogenmod;
 
 import android.content.Context;
-import android.content.ContentResolver;
 import android.content.Intent;
 import android.content.pm.UserInfo;
 import android.content.res.Resources;
@@ -42,15 +41,12 @@ import cyanogenmod.providers.CMSettings;
 import org.cyanogenmod.internal.logging.CMMetricsLogger;
 
 import static com.android.internal.util.cm.PowerMenuConstants.*;
-import com.android.settings.widget.NumberPickerPreference;
 
 import java.util.Arrays;
 import java.util.ArrayList;
 import java.util.List;
 
-public class PowerMenuActions extends SettingsPreferenceFragment
-        implements OnPreferenceChangeListener {
-
+public class PowerMenuActions extends SettingsPreferenceFragment {
     final static String TAG = "PowerMenuActions";
 
     private CheckBoxPreference mRebootPref;
@@ -76,28 +72,12 @@ public class PowerMenuActions extends SettingsPreferenceFragment
     private String[] mAvailableActions;
     private String[] mAllActions;
 
-    private NumberPickerPreference mScreenshotDelay;
-
-    private ContentResolver mCr;
-    private PreferenceScreen mPrefSet;
-
-    private static final int MIN_DELAY_VALUE = 1;
-    private static final int MAX_DELAY_VALUE = 30;
-
-
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         addPreferencesFromResource(R.xml.power_menu_settings);
         mContext = getActivity().getApplicationContext();
-
-        mPrefSet = getPreferenceScreen();
-
-        mCr = getActivity().getContentResolver();
-
-
 
         mAvailableActions = getActivity().getResources().getStringArray(
                 R.array.power_menu_actions_array);
@@ -136,20 +116,11 @@ public class PowerMenuActions extends SettingsPreferenceFragment
                 mSilentPref = (CheckBoxPreference) findPreference(GLOBAL_ACTION_KEY_ASSIST);
             }
         }
-        mScreenshotDelay = (NumberPickerPreference) mPrefSet.findPreference(
-                SCREENSHOT_DELAY);
-        mScreenshotDelay.setOnPreferenceChangeListener(this);
-        mScreenshotDelay.setMinValue(MIN_DELAY_VALUE);
-        mScreenshotDelay.setMaxValue(MAX_DELAY_VALUE);
-        int ssDelay = Settings.System.getInt(mCr,
-                Settings.System.SCREENSHOT_DELAY, 1);
-        mScreenshotDelay.setCurrentValue(ssDelay);
 
         mOnTheGoAlphaPref = (SlimSeekBarPreference) findPreference(PREF_ON_THE_GO_ALPHA);
         mOnTheGoAlphaPref.setDefault(50);
         mOnTheGoAlphaPref.setInterval(1);
         mOnTheGoAlphaPref.setOnPreferenceChangeListener(this);
-
 
         getUserConfig();
     }
